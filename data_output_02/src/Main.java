7d5098e44de3b96f8f9311601a038f51ecd101a6
Abrão Astério Junior <abcorinthians@gmail.com>
2021-02-11 02:51:22 -0300

    Demo 3 data output: Using concatenation

---
+++ data_output_02/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Locale;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+
+		double x = 10.35784;
+
+		System.out.println("Result = " + x + " meters"); // General rule for print and println
+
+		System.out.printf("Result = %.2f meters%n", x);
+	}
+}
