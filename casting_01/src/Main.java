8a1bda58af5a6574db90abf7a10d13a312b5c2b2
Abrão Astério Junior <abcorinthians@gmail.com>
2021-02-11 19:08:51 -0300

    Demo 1 Casting

---
+++ casting_01/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int a, b;
+		double result;
+
+		a = 5;
+		b = 2;
+
+		result = (double) a / b; // Using casting
+
+		System.out.println(result); // OUTPUT: 2.5
+	}
+}
