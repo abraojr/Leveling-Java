85ee1b1c953750e5e7c67032dc86e2d2131146f7
Abrão Astério Junior <abcorinthians@gmail.com>
2021-02-11 02:34:16 -0300

    Demo 2 data output: Using Locale

--- data_output_01/src/Main.java
+++ data_output_01/src/Main.java
@@ -1,11 +1,17 @@
+import java.util.Locale;
 
 public class Main {
 
 	public static void main(String[] args) {
 
+		Locale.setDefault(Locale.US);
+
 		int y = 32;
+		double x = 10.35784;
 
 		System.out.println(y);
+		System.out.println(x);
+		System.out.printf("%.2f%n", x);//Number with two decimal places
+		System.out.printf("%.4f%n", x);//Number with four decimal places
 	}
-
 }
