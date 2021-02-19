f9dd04413b2c1ee2fd6dea660ff4367cdd34ccf1
Abrão Astério Junior <abcorinthians@gmail.com>
2021-02-19 01:05:21 -0300

    Demo 1 of bitwise operators

---
+++ bitwise_operators_01/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int n1 = 89;
+		int n2 = 60;
+		System.out.println(n1 & n2); // OUTPUT:24
+		System.out.println(n1 | n2); // OUTPUT:125
+		System.out.println(n1 ^ n2); // OUTPUT:101
+
+	}
+}
